5b84f97c7450608699f347b1d5a8dc0d3aed8743
Пётр Меренков <peter.merenkov@gmail.com>
2021-11-26 17:00:50 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         String mainStr = "add 1 Пупкин Василий Иванович 2 4 ИАИТ 26.11.2021";
 
         int index = 0;
@@ -106,5 +113,20 @@ public class Main {
         System.out.println(command + " " + id1 + " " + surname + " "
                 + firstname + " " + patronymic + " " + id2 + " " + number
                 + " " + faculty + " " + day + "." + month + "." + year);
+
+        //-------------------------------------------------------------------------
+
+        JSONParser parser = new JSONParser();
+        FileReader reader = new FileReader("test.json");
+
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+
+        String modelName = (String) jsonObject.get("name");
+        Integer modelId1 = ((Long) jsonObject.get("id")).intValue();
+        Integer modelId2 = ((Long) jsonObject.get("group.id")).intValue();
+
+        System.out.println(modelId2);
+
+        reader.close();
     }
 }
