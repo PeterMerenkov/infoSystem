9ae9906ea56eaa558d5f778f0fc31d05cfe4ec52
Пётр Меренков <peter.merenkov@gmail.com>
2021-12-06 12:17:19 +0400

    add index parsing & create switcher

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -5,8 +5,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Controller {
-    private Model model;
-    private View view;
+    private final Model model;
+    private final View view;
 
     Controller(Model model, View view) {
         this.model = model;
@@ -14,107 +14,128 @@ public class Controller {
     }
 
     //command id1 fio id2 number fac date
-    //add 1 Пупкин Василий Иванович 2 4 ИАИТ 26.11.2021
+    //add 0 1 Пупкин Василий Иванович 2 4 ИАИТ 26.11.2021
     public void command (String str) {
-        int index = 0;
-        String temp = "";
+        //str parser begin-------------------
+        int i = 0;
+        StringBuilder temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        String command = temp;
+        String command = temp.toString();
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        BigInteger id1 = new BigInteger(temp);
+        int index = Integer.parseInt(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        String fio = temp;
+        BigInteger id1 = new BigInteger(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
+        }
+        String fio = temp.toString();
+
+        i++;
+        temp = new StringBuilder();
+
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
         fio += " " + temp;
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
         fio += " " + temp;
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        BigInteger id2 = new BigInteger(temp);
+        BigInteger id2 = new BigInteger(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        Integer number = Integer.parseInt(temp);
+        Integer number = Integer.parseInt(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != ' ') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != ' ') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        String faculty = temp;
+        String faculty = temp.toString();
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != '.') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != '.') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        int day = Integer.parseInt(temp);
+        int day = Integer.parseInt(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.charAt(index) != '.') {
-            temp += str.charAt(index);
-            index++;
+        while(str.charAt(i) != '.') {
+            temp.append(str.charAt(i));
+            i++;
         }
-        int month = Integer.parseInt(temp);
+        int month = Integer.parseInt(temp.toString());
 
-        index++;
-        temp = "";
+        i++;
+        temp = new StringBuilder();
 
-        while(str.length() != index) {
-            temp += str.charAt(index);
-            index++;
+        while(str.length() != i) {
+            temp.append(str.charAt(i));
+            i++;
         }
-        int year = Integer.parseInt(temp);
+        int year = Integer.parseInt(temp.toString());
 
         Calendar date = new GregorianCalendar(year, month - 1, day);
+        //str parser end-------------------
+
+        switch (command) {
+            case "get" -> model.getStudent(index);
+            case "set" -> model.setStudent(index,
+                    new Student(id1, fio, date, id2, number, faculty));
+            case "add" -> model.addStudent(index,
+                    new Student(id1, fio, date, id2, number, faculty));
+            case "delete" -> model.deleteStudent(index);
+            case "show" -> view.show(model.getStudent(index));
+        }
     }
 }
