6862ce3e4f4b2ce83ae9c8b107d1c78062c4af80
Пётр Меренков <peter.merenkov@gmail.com>
2021-12-07 18:09:17 +0400

    addGroup, addStudent, getGroup, getStudent

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -1,32 +1,60 @@
 package mvc;
 
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Model {
-    private ArrayList<Student> list = new ArrayList<Student>();
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+    private ArrayList<Group> groupList = new ArrayList<Group>();
 
-    public Student getStudent(int index) {
-        return list.get(index);
+    public Student getStudent(BigInteger id) {
+        for (Student student : studentList) {
+            if (student.getId().equals(id)) {
+                return student;
+            }
+        }
+        return null;
     }
 
     public void setStudent(int index, Student student) {
-        list.set(index, student);
+        studentList.set(index, student);
     }
 
-    public void addStudent(int index, Student student) {
-        list.add(student);
+    public void addStudent(Student student) {
+        studentList.add(student);
     }
 
     public void deleteStudent(int index) {
-        list.remove(index);
+        studentList.remove(index);
     }
 
-    public void read(String path) throws FileNotFoundException {
+
+    public Group getGroup(BigInteger id) {
+        for (Group group : groupList) {
+            if (group.getId().equals(id)) {
+                return group;
+            }
+        }
+        return null;
+    }
+
+    public void addGroup(Group group) {
+        groupList.add(group);
+    }
+
+
+    public void read(String path) throws IOException, ParseException {
         JSONParser parser = new JSONParser();
         FileReader reader = new FileReader(path);
+
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+
+
     }
 }
